f48045a49e4a9b765e81f92c4e4b30c904db9d40
mehulsingh072001 <mehulsingh072001@gmail.com>
2020-11-13 01:43:35 +0530

    fkdslajflk

--- client/src/components/content/Benifits.vue
+++ client/src/components/content/Benifits.vue
@@ -1,14 +1,14 @@
 <template>
   <main class="container">
     <section class="card">
-      <img src="../../assets/shopify.jpg" alt="prime">
+      <img src="../../assets/web-hosting.jpg" alt="prime">
       <div>
         <h2>Key Benifits</h2>
         <p>Consectetur delectus et possimus debitis odit Dignissimos adipisci quis tempore</p>
       </div>
     </section>
     <section class="card">
-      <img src="../../assets/shopify.jpg" alt="prime">
+      <img src="../../assets/" alt="prime">
       <div>
         <h2>Key Benifits</h2>
         <p>Consectetur delectus et possimus debitis odit Dignissimos adipisci quis tempore</p>
